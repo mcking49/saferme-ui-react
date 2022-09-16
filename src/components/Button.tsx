a8976e5ac983d3ce1100286194124fe5d746721b
Miten Chauhan <miten@chauhan.nz>
2022-09-17 01:48:30 +1200

    use styled components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react";
+import styled from "styled-components";
 
 export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 
+const StyledButton = styled.button<ButtonProps>`
+  border-color: red;
+  border-width: 2px;
+  color: cyan;
+  padding: 16px 32px;
+  border-radius: 8px;
+`
+
 export const Button: FC<ButtonProps> = ({ children, ...props }) => {
-  return <button style={{borderColor: 'red', borderWidth: "2px", color: 'green', padding: '16px', borderRadius: '8px'}} { ...props }>{children}</button>
+  return <StyledButton { ...props }>{children}</StyledButton>
 }
