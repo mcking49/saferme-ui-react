8f6cf9d10a471cecb7200e306041416487021494
Miten Chauhan <miten@chauhan.nz>
2022-11-22 12:50:35 +1300

    Add Plus Icon

---
+++ src/components/icons/plus/IconPlus.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import { DECORATORS } from '../../../utils/storybook'
+import { IconProps } from '../../../types/icon'
+
+import { IconPlus } from '.'
+
+export default {
+  title: 'Icons/Plus',
+  component: IconPlus,
+  decorators: DECORATORS,
+} as Meta<typeof IconPlus>
+
+const Template: Story<IconProps> = (args) => <IconPlus {...args} />
+
+export const Standard = Template.bind({})
+Standard.args = {
+  size: '1rem',
+  color: 'currentColor',
+}
